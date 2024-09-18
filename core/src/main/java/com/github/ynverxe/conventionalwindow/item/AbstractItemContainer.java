5c7d1b9b9d18b74e947c9dc255567530f6721001
Ynverxe <nahubar65@gmail.com>
2024-09-18 01:29:49 -0300

    feat: missing part of e4e29824516b0f5a223223f5661da72ffb3decfa refactor: now AbstractItemContainer#get can return null items

--- core/src/main/java/com/github/ynverxe/conventionalwindow/item/AbstractItemContainer.java
+++ core/src/main/java/com/github/ynverxe/conventionalwindow/item/AbstractItemContainer.java
@@ -5,8 +5,9 @@ import com.github.ynverxe.conventionalwindow.item.container.StackedItemContainer
 import com.github.ynverxe.conventionalwindow.slot.SlotIterator;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public interface AbstractItemContainer {
+public interface AbstractItemContainer<T extends MenuItem<?, ?>> {
 
   /**
    * Returns the ItemProvider associated with the specified index (from zero to {@link #maxFindableIndex()}).
@@ -16,14 +17,14 @@ public interface AbstractItemContainer {
    *
    * @see StackedItemContainer
    * @see RelativeItemContainer
-   * @see ItemProvider
+   * @see MenuItem
    *
    * @throws IndexOutOfBoundsException Depending on the implementation, this exception
    * can be thrown if the provided int identifier is less than zero, it's expected to be an item position and is higher
    * than the stored elements in the container or when it's expected to be a key
    * and is higher than the container capacity.
    */
-  @NotNull ItemProvider get(int index) throws IndexOutOfBoundsException;
+  @Nullable T get(int index) throws IndexOutOfBoundsException;
 
   /**
    * @return the stored element count or a fixed number on {@link com.github.ynverxe.conventionalwindow.item.container.RelativeItemContainer}
@@ -35,6 +36,6 @@ public interface AbstractItemContainer {
   int nonNullCount();
 
   @Contract("_, _ -> this")
-  AbstractItemContainer fill(@NotNull SlotIterator iterator, @NotNull ItemProvider provider);
+  AbstractItemContainer<T> fill(@NotNull SlotIterator iterator, @NotNull T provider);
 
 }
