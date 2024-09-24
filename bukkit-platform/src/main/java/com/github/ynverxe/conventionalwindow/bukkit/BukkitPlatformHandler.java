b1fbabece3b28bc7f5732e897288ee76817caebc
Ynverxe <nahubar65@gmail.com>
2024-09-23 23:31:55 -0300

    part of this commit 4e8568ba7de64e81c1faeecd7fd0266c3d5b5877

--- bukkit-platform/src/main/java/com/github/ynverxe/conventionalwindow/bukkit/BukkitPlatformHandler.java
+++ bukkit-platform/src/main/java/com/github/ynverxe/conventionalwindow/bukkit/BukkitPlatformHandler.java
@@ -1,5 +1,6 @@
 package com.github.ynverxe.conventionalwindow.bukkit;
 
+import com.github.ynverxe.conventionalwindow.bukkit.internal.player.WrappedMinestomPlayer;
 import com.github.ynverxe.conventionalwindow.platform.PlatformHandler;
 import java.util.Collection;
 import net.minestom.server.inventory.Inventory;
@@ -8,27 +9,38 @@ import org.jetbrains.annotations.NotNull;
 
 public class BukkitPlatformHandler implements PlatformHandler<Player> {
 
+  /*
   private final org.bukkit.inventory.Inventory backingInventory;
 
   public BukkitPlatformHandler(org.bukkit.inventory.Inventory backingInventory) {
     this.backingInventory = backingInventory;
   }
+   */
 
   @Override
   public void render(@NotNull Player viewer, @NotNull Inventory inventory) {
-    viewer.openInventory(backingInventory);
+    WrappedMinestomPlayer wrappedMinestomPlayer = WrappedMinestomPlayer.compute(viewer);
+    wrappedMinestomPlayer.openInventory(inventory);
   }
 
   @Override
   public void remove(@NotNull Player viewer, @NotNull Inventory inventory) {
-    viewer.closeInventory();
+    WrappedMinestomPlayer wrappedMinestomPlayer = WrappedMinestomPlayer.compute(viewer);
+    wrappedMinestomPlayer.closeInventory();
   }
 
   @Override
   public Collection<Player> viewers(@NotNull Inventory inventory) {
-    return backingInventory.getViewers().stream()
+    /*
+     return backingInventory.getViewers().stream()
         .filter(humanEntity -> humanEntity instanceof Player)
         .map(humanEntity -> (Player) humanEntity)
+        .toList();
+     */
+
+    return inventory.getViewers()
+        .stream()
+        .map(player -> ((WrappedMinestomPlayer) player).bukkitPlayer())
         .toList();
   }
 }
