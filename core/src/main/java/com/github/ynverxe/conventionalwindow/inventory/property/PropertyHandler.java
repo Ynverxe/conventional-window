1b82f0ebe135f57a6a4c54a49c32e6bab47288e1
Ynverxe <nahubar65@gmail.com>
2024-10-20 13:12:05 -0300

    refactor: remove platform adaptation logic

--- core/src/main/java/com/github/ynverxe/conventionalwindow/inventory/property/PropertyHandler.java
+++
@@ -1,9 +0,0 @@
-package com.github.ynverxe.conventionalwindow.inventory.property;
-
-import net.minestom.server.inventory.InventoryProperty;
-import org.jetbrains.annotations.NotNull;
-
-public interface PropertyHandler {
-
-  boolean handleProperty(@NotNull InventoryProperty property, short value);
-}
