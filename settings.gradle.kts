fc40ac0a0e95f139893caa0e3b702663ff0cf2d4
Ynverxe <nahubar65@gmail.com>
2024-10-20 13:00:30 -0300

    delete bukkit-platform modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,14 +8,4 @@ pluginManagement {
 rootProject.name = "conventional-window"
 
 include("core")
-include("item-lib")
 include("demo")
-include("bukkit-platform")
-include("bukkit-platform:nms-1.20")
-findProject(":bukkit-platform:nms-1.20")?.name = "nms-1.20"
-include("bukkit-platform:nms-1.20.5")
-findProject(":bukkit-platform:nms-1.20.5")?.name = "nms-1.20.5"
-include("bukkit-platform:nms-common")
-findProject(":bukkit-platform:nms-common")?.name = "nms-common"
-include("bukkit-platform:nms-1.21")
-findProject(":bukkit-platform:nms-1.21")?.name = "nms-1.21"
